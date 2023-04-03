810aeb109032089d596cc17b9b42e2bb1c864ce3
Ethan <115187646+noliaphyta@users.noreply.github.com>
2023-04-03 03:18:34 -0400

    Add files via upload

---
+++ brightness.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Brightness < Formula
+  desc "Command-line display brightness control for macOS."
+  homepage "https://github.com/nriley/brightness"
+  url "https://github.com/noliaphyta/brightness/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "2028f4e174b8f01eeec3b47799208650b6534c949e1222bf93c42379523640b9"
+  license "BSD-2"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test brightness`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
